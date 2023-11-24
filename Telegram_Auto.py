44790f7c4e778ca0a56f374e6c2c8e5a2b81350e
jungwoo3490 <jungwoo3490@naver.com>
2023-11-24 21:46:26 +0900

    Telegram 자동 메시지 전송 프로그램

---
+++ Telegram_Auto.py
@@ -0,0 +1,32 @@
+import telegram
+import asyncio
+import schedule
+import time
+from datetime import datetime
+import pytz
+
+# 토큰 주석 처리
+# token = "6722824989:AAFcr_3QSlHeaRG3EHSl_WFZhYpU0CRWSw0"
+bot = telegram.Bot(token=token)
+chat_id = "-1002143232599"
+
+async def send_message():
+    now = datetime.now(pytz.timezone('Asia/Seoul')).strftime("%Y-%m-%d %H:%M:%S")
+    text = f'현재 시간: {now}'
+    await bot.sendMessage(chat_id=chat_id, text=text)
+
+
+def job():
+    now = datetime.now(pytz.timezone('Asia/Seoul'))
+    if 6 <= now.hour < 23:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(send_message())
+
+
+
+schedule.every(30).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
